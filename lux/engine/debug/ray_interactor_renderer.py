89bab4a6f9585a30e031059baed61dc79eee8d87
DragonMoffon <happydog.aja@gmail.com>
2024-03-27 14:39:33 +1300

    Ray Interactor Renderer

---
+++ lux/engine/debug/ray_interactor_renderer.py
@@ -0,0 +1,29 @@
+from arcade import draw_line, draw_point
+
+from lux.engine.interactors import RayInteractor, RayInteractorEdge
+
+
+class RayInteractorRenderer:
+
+    def __init__(self, child: RayInteractor):
+        self.child: RayInteractor = child
+
+    def draw(self):
+        edges = self.child.bounds
+        colour = self.child.colour.to_int_color()
+        position = self.child.origin
+        heading = self.child.direction.heading
+        for edge in edges:
+            start, end, center, normal = (
+                position + edge.start.rotate(heading),
+                position + edge.end.rotate(heading),
+                position + edge.center.rotate(heading),
+                edge.normal.rotate(heading)
+            )
+
+            draw_line(start.x, start.y, end.x, end.y, colour, 1.0)
+            draw_line(center.x, center.y, center.x + normal.x * 15.0, center.y + normal.y * 15.0, colour, 1.0)
+            draw_point(start.x, start.y, colour, 4.0)
+
+    def update_child(self, new_child: RayInteractor):
+        self.child = new_child
