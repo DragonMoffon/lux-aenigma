e9e02a8af2e882014a54104b6983a8f6686c99a5
DragonMoffon <happydog.aja@gmail.com>
2024-03-27 13:04:08 +1300

    Update Ray

--- lux/engine/lights/ray.py
+++ lux/engine/lights/ray.py
@@ -10,3 +10,20 @@ class Ray(NamedTuple):
     direction: Vec2
     length: float
     colour: LuxColour
+
+    def change_source(self, new_source: Vec2):
+        return Ray(new_source, self.direction, self.length, self.colour)
+
+    def change_direction(self, new_dir: Vec2):
+        return Ray(self.source, new_dir, self.length, self.colour)
+
+    def change_length(self, new_length: float):
+        return Ray(self.source, self.direction, new_length, self.colour)
+
+    def change_colour(self, new_colour: LuxColour):
+        return Ray(self.source, self.direction, self.length, new_colour)
+
+    def mask(self, mask_colour: LuxColour):
+        return Ray(self.source, self.direction, self.length, self.colour.mask(mask_colour))
+
+
