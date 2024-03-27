2ed6a3d4ef9f754706f43aaed7017f89f6d1bf33
DragonMoffon <happydog.aja@gmail.com>
2024-03-27 20:36:56 +1300

    start on scene shenanigans

---
+++ lux/engine/scene/scene_object.py
@@ -0,0 +1,13 @@
+from typing import Protocol
+
+from pyglet.math import Vec2
+
+from lux.engine.colour import LuxColour
+
+
+class SceneObject(Protocol):
+    colour: LuxColour
+    Origin: Vec2
+    Direction: Vec2
+
+
