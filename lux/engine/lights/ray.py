3adbdd748d645b9454209c2fd0179e20d544898c
DigiDuncan <digiduncan@gmail.com>
2024-04-03 20:36:14 -0400

    move ray interactions on the Ray

--- lux/engine/lights/ray.py
+++ lux/engine/lights/ray.py
@@ -1,10 +1,12 @@
 from __future__ import annotations
+import heapq
 from logging import getLogger
 from typing import NamedTuple, TYPE_CHECKING
 
 from pyglet.math import Vec2
 
 from lux.engine.colour import LuxColour
+from lux.util.maths import get_segment_intersection
 if TYPE_CHECKING:
     from lux.engine.interactors import RayInteractor, RayInteractorEdge
 
@@ -39,6 +41,37 @@ class Ray(NamedTuple):
 
     def change_strength(self, new_strength: float):
         return Ray(self.source, self.direction, self.length, new_strength)
+
+    def calculate_ray_interaction(self, interactors: tuple[RayInteractor, ...]) -> tuple[Vec2, RayInteractorEdge, RayInteractor] | None:
+        intersecting_edges = []
+
+        ray_start = self.source
+        ray_end = ray_start + self.direction * self.length
+
+        for interactor in interactors:
+            position = interactor.origin
+            heading = interactor.direction.heading
+            for edge in interactor.bounds:
+                start, end = (
+                    position + edge.start.rotate(heading),
+                    position + edge.end.rotate(heading),
+                )
+
+                interaction_point = get_segment_intersection(ray_start, ray_end, start, end)
+                if interaction_point is None:
+                    continue
+
+                diff = (interaction_point - ray_start)
+                dist = diff.dot(diff)
+
+                heapq.heappush(intersecting_edges, (dist, len(intersecting_edges), interaction_point, edge, interactor))
+
+        if not intersecting_edges:
+            return None
+
+        closest_edge = heapq.heappop(intersecting_edges)
+
+        return closest_edge[2:]
 
     def __str__(self) -> str:
         return f"Ray<({round(self.source.x, 3)}, {round(self.source.y, 3)}), dir ({round(self.direction.x, 3)}, {round(self.direction.y, 3)}), len {self.length}, str {self.strength}>"
