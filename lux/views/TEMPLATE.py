1bef0172ae72af4972b4f7d3d61d7fdd3909d5ed
DigiDuncan <digiduncan@gmail.com>
2024-04-10 23:04:19 -0400

    added a TEMPLATE view

---
+++ lux/views/TEMPLATE.py
@@ -0,0 +1,13 @@
+from lux.util.view import LuxView
+
+
+class REPLACEView(LuxView):
+
+    def __init__(self, back: LuxView):
+        super().__init__(back=back)
+
+    def on_key_press(self, symbol: int, modifiers: int):
+        super().on_key_press(symbol, modifiers)
+
+    def on_draw(self):
+        self.clear()
