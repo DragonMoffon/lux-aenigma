d8405f8037f27d31f0f0cb993ac0c3db23f7940c
DigiDuncan <digiduncan@gmail.com>
2024-04-13 04:12:27 -0400

    clean up view loading

--- lux/views/main_menu.py
+++ lux/views/main_menu.py
@@ -1,13 +1,14 @@
 import logging
+from typing import Optional, Type
 
 from arcade import key
 
 from lux.util.dev_menu import DevMenu
 from lux.util.view import LuxView
 from lux.views.musicmixer import MusicMixerView
-from lux.views.scene import SceneView
-from lux.views.editor import SceneEditorView
-from lux.views.scene_select import SceneSelectView
+# from lux.views.scene import SceneView
+# from lux.views.editor import SceneEditorView
+# from lux.views.scene_select import SceneSelectView
 from lux.views.tri_test import TriTestView
 from lux.views.fast_test import FastTestView
 from lux.views.player import PlayerTestView
@@ -18,40 +19,30 @@ from time import time
 logger = logging.getLogger("lux")
 
 
+def load_views(unloaded: dict[str, Type[LuxView]], back: Optional[LuxView]) -> dict[str, LuxView]:
+    d = {}
+    for s, v in unloaded.items():
+        t = time()
+        loaded = v(back = back)
+        print(f"{s}: {time() - t:.3f}s")
+        d[s] = loaded
+    return d
+
+
 class MenuView(LuxView):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
-        t = time()
-        test = FastTestView(back = self)
-        print(f"test: {time() - t}s")
+        views: dict[str, Type[LuxView]] = {
+            "Test": FastTestView,
+            "Player": PlayerTestView,
+            "Triangle": TriTestView,
+            "Music": MusicMixerView,
+            "Seven Segment Display": SevenSegTestView
+        }
 
-        t = time()
-        player = PlayerTestView(back = self)
-        print(f"player: {time() - t}s")
-
-        t = time()
-        tri = TriTestView(back = self)
-        print(f"triangle: {time() - t}s")
-
-        t = time()
-        mus = MusicMixerView(back = self)
-        print(f"music: {time() - t}s")
-
-        t = time()
-        seven = SevenSegTestView(back = self)
-        print(f"seven seg: {time() - t}s")
-
-        self.menu = DevMenu({
-            "Test": test,
-            "Player": player,
-            "Triangle": tri,
-            # "Scene": SceneView(back = self),
-            # "Editor": SceneEditorView(back = self),
-            # "Scene Select": SceneSelectView(back = self),
-            "Music Mixer": mus,
-            "Seven Segment Display": seven
-        })
+        loaded = load_views(views, self)
+        self.menu = DevMenu(loaded)
 
     def on_key_press(self, symbol: int, modifiers: int):
         match symbol:
